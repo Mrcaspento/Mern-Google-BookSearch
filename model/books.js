74061af6df1359f12968f0eeefd14c7e805d6707
Caspento <Mrcaspento@gmail.com>
2021-03-04 14:51:46 -0500

    creating controller for books

---
+++ model/books.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: {type: String, required: true},
+  authors: {type: String, required: true},
+  description: String,
+  image: String,
+  link: String,
+});
+
+const GoogleBooks = mongoose.model("GoogleBooks", bookSchema);
+module.exports = GoogleBooks;
